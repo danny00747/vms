6953a04949733b5c09b6282e9fcca2510b50f7e8
danny00747 <morganz55057@gmail.com>
2021-01-25 03:30:31 +0100

    added front-end

---
+++ backend/src/main/resources/db/migration/V1__ddl.sql
@@ -0,0 +1,47 @@
+/*
+ CREATE DOMAIN email_regex AS VARCHAR(255)
+    CHECK (VALUE ~ '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$');
+
+CREATE DOMAIN username_regex AS VARCHAR(25)
+    CHECK (VALUE ~ '^(?>[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*)|(?>[_.@A-Za-z0-9-]+)$');
+
+ */
+
+DROP DOMAIN IF EXISTS dRoles CASCADE;
+DROP TABLE IF EXISTS teacher CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS user_roles CASCADE;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE DOMAIN DRoles VARCHAR(25) CHECK (VALUE IN ('ROLE_ADMIN', 'ROLE_USER', 'ROLE_ANONYMOUS'));
+
+CREATE TABLE teacher
+(
+    id    uuid DEFAULT uuid_generate_v4() primary key,
+    email VARCHAR(255) NOT NULL,
+    name  VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users
+(
+    id       uuid DEFAULT uuid_generate_v4() primary key,
+    username VARCHAR(32)  NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE roles
+(
+    name DRoles primary key
+);
+
+CREATE TABLE user_roles
+(
+    role_name VARCHAR(25),
+    user_id   uuid,
+    PRIMARY KEY (role_name, user_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (role_name) REFERENCES roles (name) ON UPDATE CASCADE ON DELETE CASCADE
+
+);
+
