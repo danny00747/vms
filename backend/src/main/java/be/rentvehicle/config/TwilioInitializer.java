0d57067649398277339fa85c90db7e40992b33bf
danny00747 <morganz55057@gmail.com>
2021-05-30 03:51:41 +0200

    added SecurityUtilsUnitTest file

--- backend/src/main/java/be/rentvehicle/config/TwilioInitializer.java
+++ backend/src/main/java/be/rentvehicle/config/TwilioInitializer.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("prod")
+// @Profile("prod")
 @Configuration
 public class TwilioInitializer {
 
